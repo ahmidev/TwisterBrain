e78b45416492a7e9ce8c0619fda9f2f0e094e21a
rickaed <cedric.blesch@sncf.fr>
2023-02-07 09:33:44 +0100

    try

--- src/app/services/players.service.ts
+++ src/app/services/players.service.ts
@@ -6,6 +6,8 @@ import { Player } from '../models/player-model';
   providedIn: 'root'
 })
 export class PlayersService {
+
+  //creation de joueur en dur
   players: Player[] = [
     // {
     //   id: 0,
@@ -32,42 +34,51 @@ export class PlayersService {
     //   score: 20
     // }
   ];
-  playerList=new BehaviorSubject(this.players)
 
-  addPlayer(pseudo:string, avatar:string,){
-   
-    const player:Player ={id:this.players.length+1, pseudo:pseudo,avatar:avatar,score: 0}
+  // parti Ahmid
+  playerList = new BehaviorSubject(this.players)
+
+  addPlayer(pseudo: string, avatar: string,) {
+
+    const player: Player = { id: this.players.length + 1, pseudo: pseudo, avatar: avatar, score: 0 }
     this.players.push(player);
-   
+
   }
 
-  removePlayer(i:number){
-    this.players.splice(i,1)
+  removePlayer(i: number) {
+    this.players.splice(i, 1)
   }
+
+  // partie ced
+  // récuperer les info joueur en dur
   getAllPlayers(): Player[] {
-    
+
     return this.players;
   }
-
+  // afficher un joueur unique par id 
   getPlayerById(playerId: number): Player {
     const player = this.players.find(player => player.id === playerId);
+    //si joueur n'existe pas ("!")
     if (!player) {
+      //alors renvoie un message d'erreur
       throw new Error("player not found");
     } else {
       return player
     }
   }
 
+  // manipulation du score du joueur par selection de l'id
+  // si win a la question
   scorePlayer(playerId: number, win: boolean) {
     const player = this.getPlayerById(playerId);
     if (win) {
       player.score += 10;
     }
-
   }
+
   winnerPlayer() {
-// parcourir les joueurs
-// si score superieur aux autres
+    // parcourir les joueurs
+    // si score superieur aux autres
 
     return this.players[1]
 
